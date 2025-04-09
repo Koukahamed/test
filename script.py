065c6522f2d3501d7acff24261e8c3ea4ccf337a
Hamed <hamedsavadogo@yahoo.com>
2025-04-09 16:44:07 +0200

    ajout de la fonction somme dans le script python

--- script.py
+++ script.py
@@ -5,3 +5,9 @@ def conv_euro_xof(nb:float):
     conv = nb * 655.957
     return print(f"{nb}€ -->  {conv} FCFA")
 conv_euro_xof(25)
+
+
+def somme(x:int, y:int):
+    return print(f"la somme de {x} + {y} est : ", x+y)
+
+somme(12, 34)
